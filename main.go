84034c0152c16347ded100a29ee1fc95a7708243
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-21 05:14:13 +0900

    Qiitaコピペ

--- main.go
+++ main.go
@@ -2,8 +2,80 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
 )
 
+var (
+	Token     string
+	BotPrefix = "!"
+)
+
+func ReadConfig() error {
+	fmt.Println("Reading config file...")
+	Token = os.Getenv("LINE_BOT_TOKEN")
+
+	return nil
+
+}
+
+var BotId string
+
+// var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	goBot.Identify.Intents = discordgo.IntentMessageContent
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	BotId = u.ID
+
+	goBot.AddHandler(messageHandler)
+
+	goBot.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = goBot.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Bot is running !")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotId {
+		return
+	}
+
+	if m.Content == BotPrefix+"ping" {
+		fmt.Println("sending")
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
+
 func main() {
-	fmt.Println("main.go")
+	err := ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	Start()
+
+	<-make(chan struct{})
 }
