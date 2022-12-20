bc4c318c18a781c060bf1ea06a6138ce3f9ce0a8
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-21 00:30:41 +0900

    initial commit

---
+++ .devcontainer/gotools.sh
@@ -0,0 +1,7 @@
+go install github.com/cweill/gotests/gotests@v1.6.0
+go install github.com/fatih/gomodifytags@v1.16.0
+go install github.com/josharian/impl@v1.1.0
+go install github.com/haya14busa/goplay/cmd/goplay@v1.0.0
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
+go install golang.org/x/tools/gopls@latest
