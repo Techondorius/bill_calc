bbc794a5ad0eb7f6f5bc975bc7292bcea680ace7
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-21 10:12:12 +0900

    もう少しきれいにした

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,25 +2,30 @@
   <h1>Hi!</h1>
   <input v-model="a" />
   <p>{{ a }}</p>
-  <button @click="req">asdf</button>
-  <p v-if="{resp}">{{ resp }}</p>
+  <button @click="updateReq">asdf</button>
+  <p v-if="{ resp }">{{ resp.message }}</p>
 </template>
 
 <script setup lang="ts">
 const a = ref("Hello Word")
+
 interface res {
   message: string
 }
+
 const req = async () => {
   const url = "http://localhost:8080"
   const res = await fetch(url, {
     method: "POST",
   })
   const resData: res = await res.json()
-  resp.value = resData.message
+  return resData
+}
+const updateReq = async () => {
+  resp.value = await req()
 }
 
-const resp = ref("")
+const resp = ref(await req())
 </script>
 
 <style></style>
