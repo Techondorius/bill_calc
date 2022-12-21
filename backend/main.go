92509fe612b62704546e0e4b920401d149ba42a0
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-21 09:51:46 +0900

    ginでレスポンスするようにした

--- backend/main.go
+++ backend/main.go
@@ -2,80 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
-
-var (
-	Token     string
-	BotPrefix = "!"
-)
-
-func ReadConfig() error {
-	fmt.Println("Reading config file...")
-	Token = os.Getenv("LINE_BOT_TOKEN")
-
-	return nil
-
-}
-
-var BotId string
-
-// var goBot *discordgo.Session
-
-func Start() {
-	goBot, err := discordgo.New("Bot " + Token)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	goBot.Identify.Intents = discordgo.IntentMessageContent
-
-	u, err := goBot.User("@me")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	BotId = u.ID
-
-	goBot.AddHandler(messageHandler)
-
-	goBot.Identify.Intents = discordgo.IntentsGuildMessages
-
-	err = goBot.Open()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println("Bot is running !")
-}
-
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == BotId {
-		return
-	}
-
-	if m.Content == BotPrefix+"ping" {
-		fmt.Println("sending")
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
-	}
-}
 
 func main() {
-	err := ReadConfig()
+	g := gin.Default()
+	// Cors
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	// config.AllowOrigins = []string{"http://0.0.0.0:3000", "http://localhost:3000"}
+	g.Use(cors.New(config))
+	g.GET("/", index)
+	g.POST("/", index)
+	g.Run()
+	fmt.Println("Hello")
+}
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	Start()
-
-	<-make(chan struct{})
+func index(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello",
+	})
 }
