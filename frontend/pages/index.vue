e46d55de90113672440fb7f690fc05f123194635
Kyosuke Fujita <kurofuku01@gmail.com>
2022-12-21 09:52:57 +0900

    postできるようにした

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,10 +2,25 @@
   <h1>Hi!</h1>
   <input v-model="a" />
   <p>{{ a }}</p>
+  <button @click="req">asdf</button>
+  <p v-if="{resp}">{{ resp }}</p>
 </template>
 
 <script setup lang="ts">
 const a = ref("Hello Word")
+interface res {
+  message: string
+}
+const req = async () => {
+  const url = "http://localhost:8080"
+  const res = await fetch(url, {
+    method: "POST",
+  })
+  const resData: res = await res.json()
+  resp.value = resData.message
+}
+
+const resp = ref("")
 </script>
 
 <style></style>
