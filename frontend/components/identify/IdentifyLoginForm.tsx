92d8bb3a16a472ff85698206b97196d9070275d7
Kyosuke Fujita <kurofuku01@gmail.com>
2023-01-11 14:39:05 +0900

    ログインページ実装

--- frontend/components/identify/IdentifyLoginForm.tsx
+++ frontend/components/identify/IdentifyLoginForm.tsx
@@ -1,13 +1,28 @@
 import { Button, TextField } from '@mui/material'
+import axios, { AxiosError } from 'axios'
 import { MyButton } from 'components/button/MyButton'
 import { useState } from 'react'
 
 const IdentifyLoginForm = () => {
   const [uid, setUid] = useState('')
   const [pwd, setPwd] = useState('')
+  interface loginRes {
+    token: string
+  }
+  interface loginResError {
+    error: string
+  }
   const submit = async () => {
     await console.log(uid)
     await console.log(pwd)
+    axios
+      .post<loginRes>('http://localhost:8080/login', { userid: uid, password: pwd })
+      .then((res) => {
+        console.log(res.data.token)
+      })
+      .catch((e: AxiosError<loginResError>) => {
+        console.log(e.response)
+      })
   }
   return (
     <>
